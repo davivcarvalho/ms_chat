2b30e8e2c30378b09c31ded95e454ea980c43446
Davi Carvalho <caminhounico@gmail.com>
2021-09-29 20:50:21 -0300

    created and applied env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { FileStreamController } from './helpers/fileStream.provider'
 import { MessagesModule } from './messages/messages.module'
@@ -8,19 +9,27 @@ import { UsersModule } from './users/users.module'
 @Module({
   controllers: [FileStreamController],
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'chat',
-      autoLoadEntities: true,
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_SCHEMA'),
+        autoLoadEntities: true,
+        synchronize: true
+      }),
+      inject: [ConfigService]
     }),
     MessagesModule,
     RoomsModule,
-    UsersModule
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV !== 'production' ? '.development.env' : '.env'
+    })
   ]
 })
 export class AppModule {}
