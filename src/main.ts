fdcddea96e8e6024bcde60abfa398a017fff3981
Davi Carvalho <caminhounico@gmail.com>
2022-03-22 21:41:47 -0300

    using env

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,7 @@ async function bootstrap() {
   app.connectMicroservice({
     transport: Transport.REDIS,
     options: {
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
-      password: process.env.REDIS_PASSWORD
+      url: process.env.REDIS_URL
     }
   })
   app.register(FastifyMultpartAdapter)
