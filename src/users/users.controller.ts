4867bf98b956f9eb2ed7d6e17b3c5aa210e5ebd6
Davi Carvalho <caminhounico@gmail.com>
2022-04-02 12:13:56 -0300

    fix payload format

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,18 +3,23 @@ import { EventPattern, Payload } from '@nestjs/microservices'
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
+import { KafkaMessage } from 'kafkajs'
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @EventPattern('chat.users.created')
-  async create(@Payload() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto)
+  async create(@Payload() body) {
+    const data = body.value as CreateUserDto
+
+    return this.usersService.create(data)
   }
 
   @EventPattern('chat.users.updated')
-  async update(@Payload() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(updateUserDto.id, updateUserDto)
+  async update(@Payload() body) {
+    const data = body.value as UpdateUserDto
+
+    return this.usersService.update(data.id, data)
   }
 }
